0e3ac2cd9ee78798ca9f1ceebe38710b4dd29392
huamanie <edwinhuamani@edwins-mbp.mynetworksettings.com>
2026-06-05 00:44:16 -0400

    NavBar added (responsive)

---
+++ components/NavBar.tsx
@@ -0,0 +1,31 @@
+import Logo from '../public/NEW OL Logo (transparent).png' 
+import Image from 'next/image'
+
+export default function NavBar() {
+    return (
+        <div className='grid grid-cols-3 items-center h-16 bg-main-color'>
+            <div className='justify-self-left md:hidden'>
+                Goods
+            </div>
+
+            <div className='hidden md:flex md:justify-self-end md:gap-20 text-black'>
+                <button>
+                    Merch
+                </button>
+                <button>
+                    Shows/Music
+                </button>
+            </div>
+            <Image 
+                src={Logo}
+                alt="Our cool logo that cannot load :("
+                width={64}
+                className='justify-self-center'
+            />
+            <button className='hidden md:block md:justify-self-start md:text-black'>
+                Contact Us
+            </button>
+        </div>
+    )
+
+}
